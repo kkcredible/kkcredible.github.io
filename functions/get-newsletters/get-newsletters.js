451d761ade241b4ff0cdad36e2ae23bf65332f7d
Hiren <hiren@freshbits.in>
2020-06-01 10:54:56 +0530

    det directories

--- functions/get-newsletters/get-newsletters.js
+++ functions/get-newsletters/get-newsletters.js
@@ -1,18 +1,32 @@
 const fileSystem = require('fs');
 const util = require('util');
+const path = require('path');
 
 exports.handler = async (event, context) => {
   try {
-    const getNewslettersFrom = util.promisify(fileSystem.readFileSync).bind(fileSystem);
+    // const getNewslettersFrom = util.promisify(fileSystem.readFileSync).bind(fileSystem);
+    const directories = util.readdir(fileSystem.readFileSync).bind(fileSystem);
+
+    // fileSystem.readdir(path.join(), function (err, data) {
+    //   console.log(data);
+    // })
 
     return {
       statusCode: 200,
       body: JSON.stringify({
-        newsletters: await getNewslettersFrom('database/newsletters.json').then((requestResults) => {
+        newsletters: await directories(path.join()).then((requestResults) => {
           return JSON.parse(requestResults);
         })
       }),
     }
+    // return {
+    //   statusCode: 200,
+    //   body: JSON.stringify({
+    //     newsletters: await getNewslettersFrom('database/newsletters.json').then((requestResults) => {
+    //       return JSON.parse(requestResults);
+    //     })
+    //   }),
+    // }
   } catch (err) {
     return { statusCode: 500, body: err.toString() }
   }
