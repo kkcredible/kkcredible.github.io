62bb2fca58c9da4118e810ad4cfd95f3ad651b02
Hiren <hiren@freshbits.in>
2020-06-01 11:28:11 +0530

    read parent directory

--- functions/get-newsletters/get-newsletters.js
+++ functions/get-newsletters/get-newsletters.js
@@ -3,16 +3,26 @@ const util = require('util');
 
 exports.handler = async (event, context) => {
   try {
-    const getNewslettersFrom = util.promisify(fileSystem.readFile).bind(fileSystem);
+    const getNewslettersFrom = util.promisify(fileSystem.readdir).bind(fileSystem);
 
     return {
       statusCode: 200,
       body: JSON.stringify({
-        newsletters: await getNewslettersFrom('./src/database/newsletters.json').then((requestResults) => {
-          return JSON.parse(requestResults);
+        newsletters: await getNewslettersFrom('../').then((requestResults) => {
+          return requestResults;
         })
       }),
     }
+    // const getNewslettersFrom = util.promisify(fileSystem.readFile).bind(fileSystem);
+
+    // return {
+    //   statusCode: 200,
+    //   body: JSON.stringify({
+    //     newsletters: await getNewslettersFrom('./src/database/newsletters.json').then((requestResults) => {
+    //       return JSON.parse(requestResults);
+    //     })
+    //   }),
+    // }
   } catch (err) {
     return { statusCode: 500, body: err.toString() }
   }
