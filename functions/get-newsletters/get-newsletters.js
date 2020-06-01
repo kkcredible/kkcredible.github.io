97d75d04a6fb28ca741e673e1275223f892ee983
Hiren <hiren@freshbits.in>
2020-06-01 09:26:52 +0530

    add more nested

--- functions/get-newsletters/get-newsletters.js
+++ functions/get-newsletters/get-newsletters.js
@@ -8,7 +8,7 @@ exports.handler = async (event, context) => {
     return {
       statusCode: 200,
       body: JSON.stringify({
-        newsletters: await getNewslettersFrom('../../database/newsletters.json').then((requestResults) => {
+        newsletters: await getNewslettersFrom('../../../database/newsletters.json').then((requestResults) => {
           return JSON.parse(requestResults);
         })
       }),
